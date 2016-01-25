144f7c59bab1c96f3695b3ef44512e00b1eaa723
Daniel Logan <daniel.w.logan@gmail.com>
2016-01-25 01:18:59 -0500

    Updated site to work on heroku

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, request, redirect
+import os
+from flask import Flask
+from flask import render_template
+from flask import request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-  return redirect('/index')
+email_addresses = []
 
-@app.route('/index')
-def index():
-  return render_template('index.html')
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run()
