b8797ec37c1a2382426d9b2e638421b992dbb7a4
Daniel Logan <daniel.w.logan@gmail.com>
2016-02-10 19:36:44 -0500

    updated about page

--- app.py
+++ app.py
@@ -7,10 +7,14 @@ app = Flask(__name__)
 
 email_addresses = []
 
-@app.route('/')
-def hello_world():
+@app.route('/', methods=['GET', 'POST'])
+def index():
     return render_template('index.html')
+    
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run()
+    app.run(host='127.0.0.1', port=port, debug=True)
